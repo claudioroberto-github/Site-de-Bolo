e7b3f01b708b682b6846037938ca1ba56d996227
claudioroberto-github <138341915+claudioroberto-github@users.noreply.github.com>
2023-10-04 20:14:15 -0300

    Add files via upload

---
+++ trabalho de APW/src/login.js
@@ -0,0 +1,9 @@
+const botao = document.getElementById("botao");
+
+function resumoregistrar() {
+    window.location = 'Registra-se.html';
+}
+
+function botao() {
+    location.href = 'Registra-se.html';
+}
---
+++ trabalho de APW/src/main.js
@@ -0,0 +1,14 @@
+function login() {
+    window.location.href = "login.html";
+    alert("Coloque Seus Dados No Formulário.");
+}
+
+function registrar() {
+    window.location.href = "Registra-se.html";
+    alert("Coloque Seus Dados No Formulário.");
+}
+
+function vitrine() {
+    window.location.href = "vitrine.html";
+}
+
